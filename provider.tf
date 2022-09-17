010ccdbb88a15ade824c6b2e39109550484b8dfe
Pradeep <104751531+piku143526@users.noreply.github.com>
2022-09-18 02:44:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.20.0"
+    }
+  }
+}
+
 provider "google" {
   credentials = file("./terraform-sa.json")
-  project     = "noted-cider-362008"
-  region      = "us-central1"
-  version     = "~> 2.5.0"
+  project = "noted-cider-362008"
+  region  = "us-west1"
+  zone    = "us-west1-c"
 }
