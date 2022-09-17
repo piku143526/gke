b27e30813d03867b2cf68e99065d5bfbf7dc11b0
Pradeep <104751531+piku143526@users.noreply.github.com>
2022-09-18 02:23:30 +0530

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -10,3 +10,16 @@ module "network" {
   subnetwork_pods     = "10.41.0.0/16"
   subnetwork_services = "10.42.0.0/16"
 }
+  
+  
+  module "cluster" {
+  source                           = "./modules/vpc-native"
+  region                           = "us-central1"
+  name                             = "gke-example"
+  project                          = "terraform-module-cluster"
+  network_name                     = "kube"
+  nodes_subnetwork_name            = module.network.subnetwork
+  kubernetes_version               = "1.16.10-gke.8"
+  pods_secondary_ip_range_name     = module.network.gke_pods_1
+  services_secondary_ip_range_name = module.network.gke_services_1
+}
