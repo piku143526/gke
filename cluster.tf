52265a08c64085b9d8a9b17c9755564f568682a8
Pradeep <104751531+piku143526@users.noreply.github.com>
2022-09-18 02:17:16 +0530

    Create cluster.tf

---
+++ cluster.tf
@@ -0,0 +1,12 @@
+module "network" {
+  source = "./modules/network/"
+  // base network parameters
+  network_name     = "kube"
+  subnetwork_name  = "kube-subnet"
+  region           = "us-central1"
+  enable_flow_logs = "false"
+  // subnetwork primary and secondary CIDRS for IP aliasing
+  subnetwork_range    = "10.40.0.0/16"
+  subnetwork_pods     = "10.41.0.0/16"
+  subnetwork_services = "10.42.0.0/16"
+}
